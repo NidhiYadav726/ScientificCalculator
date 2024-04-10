82f2290515add3d914e417e5ecfa1c624a2d8227
yadavnidhi0520 <nidhi.yadav@knoldus.com>
2024-04-10 11:34:44 +0530

    updated code

--- src/test/java/ScientificCalculatorTest/calculatorTest.java
+++ src/test/java/ScientificCalculatorTest/calculatorTest.java
@@ -1,20 +1,23 @@
 package ScientificCalculatorTest;
+
 import org.testng.Assert;
 import org.testng.annotations.*;
 import scientificCalc.ScientificCalc_source;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ScientificCalc_test {
+public class calculatorTest {
     ScientificCalc_source calculator;
 
     @BeforeMethod
     public void setUp() {
         calculator = new ScientificCalc_source();
     }
+
     //Data provider for basicOperations
     @DataProvider(name = "testdata1")
     public Object[][] csvTestDataDataProvider1() throws IOException {
@@ -22,14 +25,15 @@ public class ScientificCalc_test {
         BufferedReader reader = new BufferedReader(new FileReader("testdata1.csv"));
         String line;
         while ((line = reader.readLine()) != null) {
-            String[] value= line.split(",");
+            String[] value = line.split(",");
             double a = Double.parseDouble(value[0]);
             double b = Double.parseDouble(value[1]);
-            testData.add(new Object[]{a,b});
+            testData.add(new Object[]{a, b});
         }
         reader.close();
         return testData.toArray(new Object[0][0]);
     }
+
     //Data Provider for testSquareRoot
     @DataProvider(name = "testdata2")
     public Object[][] csvTestDataDataProvider2() throws IOException {
@@ -37,13 +41,14 @@ public class ScientificCalc_test {
         BufferedReader reader = new BufferedReader(new FileReader("testdata2.csv"));
         String line;
         while ((line = reader.readLine()) != null) {
-            String[] value= line.split(",");
+            String[] value = line.split(",");
             int num = Integer.parseInt(value[0]);
             testData.add(new Object[]{num});
         }
         reader.close();
         return testData.toArray(new Object[0][0]);
     }
+
     //Data Provider for testPower
     @DataProvider(name = "testdata_power")
     public Object[][] csvTestDataDataProvider3() throws IOException {
@@ -51,14 +56,15 @@ public class ScientificCalc_test {
         BufferedReader reader = new BufferedReader(new FileReader("testdata_power.csv"));
         String line;
         while ((line = reader.readLine()) != null) {
-            String[] value= line.split(",");
+            String[] value = line.split(",");
             int base = Integer.parseInt(value[0]);
-            int exponent= Integer.parseInt(value[1]);
-            testData.add(new Object[]{base,exponent});
+            int exponent = Integer.parseInt(value[1]);
+            testData.add(new Object[]{base, exponent});
         }
         reader.close();
         return testData.toArray(new Object[0][0]);
     }
+
     //Data Provider for advancedOperations
     @DataProvider(name = "testdata3")
     public Object[][] csvTestDataDataProvider4() throws IOException {
@@ -66,7 +72,7 @@ public class ScientificCalc_test {
         BufferedReader reader = new BufferedReader(new FileReader("testdata3.csv"));
         String line;
         while ((line = reader.readLine()) != null) {
-            String[] value= line.split(",");
+            String[] value = line.split(",");
             double angle = Double.parseDouble(value[0]);
             testData.add(new Object[]{angle});
         }
@@ -76,56 +82,57 @@ public class ScientificCalc_test {
 
     //testAddition
     @Test(groups = "basicOperations", priority = 1, dataProvider = "testdata1")
-    public void testAddition(double a ,double b) {
+    public void testAddition(double a, double b) {
         ScientificCalc_source calculator = new ScientificCalc_source();
-        double sum = calculator.addition(a , b);
-        Assert.assertEquals(sum, a+b, "Invalid Sum");
+        double sum = calculator.addition(a, b);
+        Assert.assertEquals(sum, a + b, "Invalid Sum");
     }
 
     //testSubtraction
-    @Test(groups = "basicOperations" , priority = 2, dataProvider = "testdata1")
+    @Test(groups = "basicOperations", priority = 2, dataProvider = "testdata1")
     public void testSubtraction(double a, double b) {
         ScientificCalc_source calculator = new ScientificCalc_source();
-        double difference = calculator.subtraction(a,b );
-        Assert.assertEquals(difference, a-b, "Invalid Difference");
+        double difference = calculator.subtraction(a, b);
+        Assert.assertEquals(difference, a - b, "Invalid Difference");
     }
 
-//testMultiplication
-    @Test(groups = "basicOperations" , priority = 3, dataProvider = "testdata1")
+    //testMultiplication
+    @Test(groups = "basicOperations", priority = 3, dataProvider = "testdata1")
     public void testMultiplication(double a, double b) {
         ScientificCalc_source calculator = new ScientificCalc_source();
-        double product = calculator.multiplication(a,b);
-        Assert.assertEquals(product, a*b , "Invalid Product");
+        double product = calculator.multiplication(a, b);
+        Assert.assertEquals(product, a * b, "Invalid Product");
     }
 
-//testDivision
-    @Test(groups = "basicOperations" , priority = 4, dataProvider = "testdata1")
+    //testDivision
+    @Test(groups = "basicOperations", priority = 4, dataProvider = "testdata1")
     public void testDivision(double a, double b) {
         ScientificCalc_source calculator = new ScientificCalc_source();
         double div = calculator.division(a, b);
-        Assert.assertEquals(div, a/b , "Invalid Division");
+        Assert.assertEquals(div, a / b, "Invalid Division");
     }
 
-//testSquareRoot
-    @Test(groups = "advancedOperations" , priority = 5, dataProvider = "testdata2")
+    //testSquareRoot
+    @Test(groups = "advancedOperations", priority = 5, dataProvider = "testdata2")
     public void testSquareroot(int num) {
         ScientificCalc_source calculator = new ScientificCalc_source();
         int squareroot = calculator.squareRoot(num);
         Assert.assertEquals(squareroot, Math.sqrt(num), "Invalid Squareroot");
     }
 
-//testPower
-    @Test(groups = "advancedOperations" , priority = 6, dataProvider = "testdata_power")
+    //testPower
+    @Test(groups = "advancedOperations", priority = 6, dataProvider = "testdata_power")
     public void testPower(int base, int exponent) {
         ScientificCalc_source calculator = new ScientificCalc_source();
         int power = calculator.power(base, exponent);
-        Assert.assertEquals(power, Math.pow(base,exponent), "Invalid result");
+        Assert.assertEquals(power, Math.pow(base, exponent), "Invalid result");
     }
+
     //invalidAddition
     @Test(priority = 7)
-    public void testInvalidAddition(){
+    public void testInvalidAddition() {
         try {
-           calculator.add("akj", "deg");
+            calculator.add("akj", "deg");
             Assert.fail("Invalid addition test failed. Expected NumberFormatException, but no exception was thrown.");
         } catch (NumberFormatException e) {
             Assert.assertTrue(e.getMessage().startsWith("For input string:"), "Invalid exception message for invalid addition.");
@@ -134,9 +141,9 @@ public class ScientificCalc_test {
 
     //invalidDivision
     @Test(priority = 8)
-    public void testInvalidDivision(){
+    public void testInvalidDivision() {
         try {
-            calculator.divide(2,0);
+            calculator.divide(2, 0);
             Assert.fail("Invalid division test failed. Expected ArithmeticException, but no exception was thrown.");
         } catch (ArithmeticException e) {
             Assert.assertTrue(e.getMessage().contains("divided by zero"), "Invalid exception message for invalid division");
@@ -144,30 +151,31 @@ public class ScientificCalc_test {
     }
 
     //testSinFunction
-    @Test(groups = "advancedOperations" , priority = 9, dataProvider = "testdata3")
+    @Test(groups = "advancedOperations", priority = 9, dataProvider = "testdata3")
     public void testSinFunction(double angle) {
         ScientificCalc_source calculator = new ScientificCalc_source();
         double result = calculator.sin(angle);
         Assert.assertEquals(result, Math.sin(Math.toRadians(angle)), "Sin function failed");
     }
 
-//testCosFunction
-    @Test(groups = "advancedOperations" , priority = 10, dataProvider = "testdata3")
+    //testCosFunction
+    @Test(groups = "advancedOperations", priority = 10, dataProvider = "testdata3")
     public void testCosFunction(double angle) {
         ScientificCalc_source calculator = new ScientificCalc_source();
         double result = calculator.cos(angle);
         Assert.assertEquals(result, Math.cos(Math.toRadians(angle)), "Cos function failed");
     }
 
-//testTanFunction
-    @Test(groups = "advancedOperations" , priority = 11, dataProvider = "testdata3")
+    //testTanFunction
+    @Test(groups = "advancedOperations", priority = 11, dataProvider = "testdata3")
     public void testTanFunction(double angle) {
         ScientificCalc_source calculator = new ScientificCalc_source();
         double result = calculator.tan(angle);
         Assert.assertEquals(result, Math.tan(Math.toRadians(angle)), "Tan function failed");
     }
+
     @AfterMethod
-    public void EndOfCalculation(){
+    public void endOfCalculation() {
         System.out.println("Done with Computation ");
     }
 }
